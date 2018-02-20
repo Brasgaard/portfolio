ea2249e72d9c83b53652df83abfeaf41997265d2
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-02-20 21:40:54 +0100

    Test
    Test Css

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Title -->
+	<title>Alexander Brasgaard - Portfolio 2018</title>
+
+	<meta charset="UTF-8">
+	
+	<!-- Description -->
+	<meta name="Description" content="Alexander Brasgaard is a front-end developer based in Aarhus Denmark.">
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicons -->
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
+	<!-- Css, Sass and fonts-->
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+
+<h1>Hi there! I'm Alexander <br> and I do front-end developer from Denmark.</h1>
+<p id="ptest"> I really like UI / UX Animations </p>
+
+</body>
+</html>
