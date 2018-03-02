f217476ee50acd821adc33993fcede8a8342e32b
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-03-02 13:49:58 +0100

    HTML, CSS and JS
    Layout, structure and small features.

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <head>
 	<!-- Title -->
 	<title>Alexander Brasgaard - Portfolio 2018</title>
-
-	<meta charset="UTF-8">
 	
 	<!-- Description -->
-	<meta name="Description" content="Alexander Brasgaard is a front-end developer based in Aarhus Denmark.">
+	<meta charset="UTF-8">
+	<meta name="Description" content="I'm a multimedia design student based in Aarhus Denmark and this is my portfolio. I'm currently on my 3rd semester with my specialization in front-end delevelopment. I totally love working with this front-end it is very exciting and fun to play with.  ">
 
 	<!-- Keywords -->
 	<meta name="keywords" content="HTML,CSS,JavaScript">
@@ -15,18 +14,73 @@
 	<!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Favicons -->
-	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-
 	<!-- Css, Sass and fonts-->
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
+
+    <!-- Favicons -->
+	<link rel="icon" type="image/png" href="#" sizes="32x32" />
+	<link rel="icon" type="image/png" href="#" sizes="16x16" />
 </head>
+
 <body>
 
+<!-- Navigation start -->
+<header>
+	<div class="container-lg">
+		<nav class="globalNav">
+			<ul class="commenTxt">
+				<li>About</li>
+				<li>Portfolio</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	</div>
+</header>
 
-<h1>Hi there! I'm Alexander <br> and I do front-end developer from Denmark.</h1>
-<p id="ptest"> I really like UI / UX Animations </p>
+<div class="globalContent">
+	<main>
+		<div class="stripes">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
 
+		<section id="intro">
+			<div class="container-lg">
+				<p class="frontTxt">I'm a</p>
+				<h1 class="frontTitle">Front-end Developer</h1>
+				<p class="frontTxt">Who loves:
+					<span class="txt-rotate" data-period="2000" data-rotate='[ "UX / UI design.", "Animations.", "Clean design.", "Teamwork!" ]'></span>
+				</p>
+			</div>
+		</section>
+		
+
+<!-- About me start -->
+		<section id="aboutMe">
+			<h1>aboutMe</h1>
+		</section>
+
+<!-- Portfolio start -->
+		<section id="portfolioMe">
+			<h1>portfolioMe</h1>
+		</section>
+	</main>
+</div>
+
+<!-- Footer start -->
+<footer class="globalFooter">
+	<section id="footerSoMe">
+		<p class="footerTxt"> This is my Facebook(icon). This is my Instagram(icon). This is my Behance(icon). This is my Dribbble(icon).</p>
+	</section>
+</footer>
+
+
+
+<!-- Scripts -->
+<script src="js/scripts.js"></script>
 </body>
 </html>
