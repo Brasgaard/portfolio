b8fa9151419c052c5f8c529d694a298c268b30df
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-03-02 23:39:23 +0100

    HTML, CSS and JS.
    Alot of things.

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 		<section id="intro">
 			<div class="container-lg">
 				<p class="frontTxt">I'm a</p>
-				<h1 class="frontTitle">Front-end Developer</h1>
+				<h1 class="frontTitle">< Front-end Developer ></h1>
 				<p class="frontTxt">Who loves:
 					<span class="txt-rotate" data-period="2000" data-rotate='[ "UX / UI design.", "Animations.", "Clean design.", "Teamwork!" ]'></span>
 				</p>
@@ -59,18 +59,98 @@
 		</section>
 		
 
-<!-- About me start -->
-		<section id="aboutMe">
-			<h1>aboutMe</h1>
-		</section>
+<!-- About me start
+		<div class="container-lg">
+			<section id="aboutMe">
+				<h1 class="aboutTitle">about me</h1>
+				<p class="aboutTxt">I'm a front-end developer student <br> based in Aarhus, Denmark.</p>
+				<p class="commenTxt">I'm interested in all kinds of visual communication<br> but my major focus is on web development. I love creating stunning<br> functional and unique websites. Which brings great value<br> and a huge smile on the customers lips.</p>
+				<p class="commenTxt">In my spare time I really enjoy hanging out with friends<br> and do something fun or exciting. And obviously sitting<br> in front of my computer with music in my ears.</p>
+				<p class="commenTxt">I have always loved to explore and try out new things.<br>Being spontaneous is a big part of me. If i see a great opportunity<br> to try something out, count me in! It's important to me to keep<br> growing on a personal level together with my professional career.</p>
+			</section>
 
-<!-- Portfolio start -->
+			<section id="aboutMePhoto">
+				<img id="myPhoto" src="images/alex.png" alt="Alexander Brasgaard" title="Alexander Brasgaard">
+			</section>
+		</div>
+
+Portfolio start
 		<section id="portfolioMe">
-			<h1>portfolioMe</h1>
+			<div class="container-lg">
+				<h1 class="frontTitle">portfolioMe</h1>
+			</div>
 		</section>
 	</main>
 </div>
+-->
+<div class="container-lg">
+	<div class="grid-container">
+	  <div class="item1">
+	  	<section id="aboutMe">
+			<h1 class="aboutTitle">about me</h1>
+			<p class="aboutTxt">I'm a front-end developer student <br> based in Aarhus, Denmark.</p>
+			<p class="commenTxt">I'm interested in all kinds of visual communication<br> but my major focus is on web development. I love creating stunning<br> functional and unique websites. Which brings great value<br> and a huge smile on the customers lips.</p>
+			<p class="commenTxt">In my spare time I really enjoy hanging out with friends<br> and do something fun or exciting. And obviously sitting<br> in front of my computer with music in my ears.</p>
+			<p class="commenTxt">I have always loved to explore and try out new things.<br>Being spontaneous is a big part of me. If i see a great opportunity<br> to try something out, count me in! It's important to me to keep<br> growing on a personal level together with my professional career.</p>
+		</section>
+	  </div>
 
+	  <div class="item2">
+	  	<section id="aboutMePhoto">
+			<img id="myPhoto" src="images/alex.png" alt="Alexander Brasgaard" title="Alexander Brasgaard">
+		</section>
+	  </div>
+
+	  <div class="item3">
+	  	<section id="portfolioMe">
+	  		<h1 class="portfolioTitle">portfolio</h1>
+	  		<p class="portfolioTxt">Take a look at my portfolio which I'm proud to present.</p>
+	  	</section>
+	  </div>
+
+	  <div class="item4">
+	  	<section id="hautefriture">
+	  		<img class="portfolioPhoto" src="images/hautefriture.png" alt="Haute Friture Gourment Grill" title="Haute Friture Gourment Grill">
+	  	</section>
+	  </div>
+
+	  <div class="item5">
+	  	<section id="hautefritureTxt">
+	  		<h1 class="portleftTitle">haute friture</h1>
+	  		<p class="portleftTxt">Where grill meets gourment.</p>
+	  		<p class="commenTxt">In this project I worked together with my study group to create<br> a new website for Haute Friture. They didn't have a website and<br> wanted to be able to show their current and new customers what<br> their gourment grill looked like. The focus was also to how focus on<br> their unique food through visuelle element and a menu card.</p>
+	  	</section>
+	  </div>
+
+	  <div class="item6">
+	  	<section id="njordex">
+	  		<img class="portfolioPhoto" src="images/njordex.png" alt="Njordex" title="Njordex">
+	  	</section>
+	  </div>
+
+	  <div class="item7">
+	  	<section id="njordexTxt">
+	  		<h1 class="portrightTitle">njordex</h1>
+	  		<p class="portrightTxt">Denmark's first cryptocurrency exchange.</p>
+	  		<p class="commenTxt">This project my job was to create the visual identity for Njordex.<br>This include a landing page and a full website design with 10+ sites.<br>The goal was to create an easy overview which instantly builded trust.<br>One of the biggest focuses was to keep the site userfriendly everywhere.</p>
+	  	</section>
+	  </div>
+
+	  <div class="item8">
+	  	<section id="nordnordnordTxt">
+	  		<h1 class="portleftTitle">nordnordnord</h1>
+	  		<p class="portleftTxt">Upcoming danish indierock band.</p>
+	  		<p class="commenTxt">Working with nordnordnord was something totally different.<br> The focus was to differentiate them from other bands and their identities.<br> The main problem was that nordnordnord really didn't know their own identity<br> very well, since they are a new band. The end result was nordnordnord's<br> landingpage was animationed in different layers. This together with sound<br> created a whole new music univers.</p>
+	  	</section>
+	  </div>
+
+	  <div class="item9">
+	  	<section id="nordnordnord">
+	  		<img class="portfolioPhoto" src="images/nordnordnord.png" alt="nordnordnord" title="nordnordnord">
+	  	</section>
+	  </div>
+	</div>
+</div>
 <!-- Footer start -->
 <footer class="globalFooter">
 	<section id="footerSoMe">
