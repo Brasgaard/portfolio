f3d6b126fd0cd5d4f8ba65a2d5a622a85e67c7cc
Brasgaard <alexanderbrasgaard@hotmail.com>
2018-03-21 02:55:54 +0100

    Re-design
    Title

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<!-- Title -->
-	<title>Alexander Brasgaard - Portfolio 2018</title>
+	<title>Alexander Brasgaard - Front-end developer</title>
 	
 	<!-- Description -->
 	<meta charset="UTF-8">
-	<meta name="Description" content="I'm a multimedia design student based in Aarhus Denmark and this is my portfolio. I'm currently on my 3rd semester with my specialization in front-end delevelopment. I totally love working with this front-end it is very exciting and fun to play with.  ">
+	<meta name="Description" content="I'm a multimedia design student based in Aarhus Denmark and this is my portfolio. I'm currently on my 3rd semester with my specialization in front-end delevelopment. I totally love working with this front-end it is very exciting and fun to play with.">
 
 	<!-- Keywords -->
 	<meta name="keywords" content="HTML,CSS,JavaScript">
@@ -16,133 +16,133 @@
 
 	<!-- Css, Sass and fonts-->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 
     <!-- Favicons -->
-	<link rel="icon" type="image/png" href="#" sizes="32x32" />
-	<link rel="icon" type="image/png" href="#" sizes="16x16" />
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
 </head>
-
 <body>
 
-<!-- Navigation start -->
-<header>
-	<div class="container-lg">
-		<nav class="globalNav">
-			<ul class="commenTxt">
-				<li>About</li>
-				<li>Portfolio</li>
-				<li>Contact</li>
-			</ul>
-		</nav>
-	</div>
-</header>
-
-<div class="globalContent">
-	<main>
-		<div class="stripes">
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-
-		<section id="intro">
-			<div class="container-lg">
-				<p class="frontTxt">I'm a</p>
-				<h1 class="frontTitle">< Front-end Developer ></h1>
-				<p class="frontTxt">Who loves:
-					<span class="txt-rotate" data-period="2000" data-rotate='[ "UX / UI design.", "Animations.", "Clean design.", "Teamwork!" ]'></span>
-				</p>
+<div class="container-fluid-1">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-xs-12">
+				<p id="nameFont" class="animated fadeInUp"><span style="font-weight: 700;" >ALEXANDER</span>BRASGAARD</p>
+  				<p id="imFont" class="animated fadeInUp">I'm a <span class="txt-rotate" data-period="5000" data-rotate='[ "< Front-end Developer >" ]'></span> and Web Designer</p>
 			</div>
-		</section>
-
-<div class="container-lg">
-	<div class="grid-container">
-	  <div class="item1">
-	  	<section id="aboutMe">
-			<h1 class="aboutTitle">about me</h1>
-			<p class="aboutTxt">I'm a front-end developer student <br> based in Aarhus, Denmark.</p>
-			<p class="commenTxt">I'm interested in all kinds of visual communication<br> but my major focus is on web development. I love creating stunning<br> functional and unique websites. Which brings great value<br> and a huge smile on the customers lips.</p>
-			<p class="commenTxt">In my spare time I really enjoy hanging out with friends<br> and do something fun or exciting. And obviously sitting<br> in front of my computer with music in my ears.</p>
-			<p class="commenTxt">I have always loved to explore and try out new things.<br>Being spontaneous is a big part of me. If i see a great opportunity<br> to try something out, count me in! It's important to me to keep<br> growing on a personal level together with my professional career.</p>
-		</section>
-	  </div>
-
-	  <div class="item2">
-	  	<section id="aboutMePhoto">
-			<img id="myPhoto" src="images/alex.png" alt="Alexander Brasgaard" title="Alexander Brasgaard">
-		</section>
-	  </div>
-
-	  <div class="item3">
-	  	<section id="portfolioMe">
-	  		<h1 class="portfolioTitle">portfolio</h1>
-	  		<p class="portfolioTxt">Take a look at my portfolio which I'm proud to present.</p>
-	  	</section>
-	  </div>
-
-	  <div class="item4">
-	  	<section id="hautefriture">
-	  		<img class="portfolioPhoto" src="images/hautefriture.png" alt="Haute Friture Gourment Grill" title="Haute Friture Gourment Grill">
-	  	</section>
-	  </div>
-
-	  <div class="item5">
-	  	<section id="hautefritureTxt">
-	  		<h1 class="portleftTitle">haute friture</h1>
-	  		<p class="portleftTxt">Where grill meets gourment.</p>
-	  		<p class="commenTxt">In this project I worked together with my study group to create<br> a new website for Haute Friture. They didn't have a website and<br> wanted to be able to show their current and new customers what<br> their gourment grill looked like. The focus was also to how focus on<br> their unique food through visuelle element and a menu card.</p>
-	  	</section>
-	  </div>
-
-	  <div class="item6">
-	  	<section id="njordex">
-	  		<img class="portfolioPhoto" src="images/njordex.png" alt="Njordex" title="Njordex">
-	  	</section>
-	  </div>
-
-	  <div class="item7">
-	  	<section id="njordexTxt">
-	  		<h1 class="portrightTitle">njordex</h1>
-	  		<p class="portrightTxt">Denmark's first cryptocurrency exchange.</p>
-	  		<p class="commenTxt">This project my job was to create the visual identity for Njordex.<br>This include a landing page and a full website design with 10+ sites.<br>The goal was to create an easy overview which instantly builded trust.<br>One of the biggest focuses was to keep the site userfriendly everywhere.</p>
-	  	</section>
-	  </div>
-
-	  <div class="item8">
-	  	<section id="nordnordnordTxt">
-	  		<h1 class="portleftTitle">nordnordnord</h1>
-	  		<p class="portleftTxt">Upcoming danish indierock band.</p>
-	  		<p class="commenTxt">Working with nordnordnord was something totally different.<br> The focus was to differentiate them from other bands and their identities.<br> The main problem was that nordnordnord really didn't know their own identity<br> very well, since they are a new band. The end result was nordnordnord's<br> landingpage was animationed in different layers. This together with sound<br> created a whole new music univers.</p>
-	  	</section>
-	  </div>
-
-	  <div class="item9">
-	  	<section id="nordnordnord">
-	  		<img class="portfolioPhoto" src="images/nordnordnord.png" alt="nordnordnord" title="nordnordnord">
-	  	</section>
-	  </div>
+		</div>
 	</div>
 </div>
 
-<div class="item10">
-	<section id="contactMe">
-		<h1 class="contactTitle">contact</h1>
-		<p class="contactTxt">Here is the different places you can contact me</p>
-		<p class="contactTxt">Alexaderbrasgaard@hotmail.com <br>+45 25 70 24 76</p>
-	</section>
+<div class="container-fluid-2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-xs-12">
+				<h3 class="titleTxtLeft ">about me</h3>
+			  	<p class="underTitleTxtLeft">I'm a front-end developer student<br>based in Aarhus, Denmark.</p>
+			  	<p class="commenTxtLeft">Hi! My name is Alexander Brasgaard.<br>I'm interested in all kinds of visual communication<br>but my major focus is on web development. I love creating stunning<br> functional and unique websites. Which brings great value<br>and a huge smile on the customers lips.</p>
+			  	<p class="commenTxtLeft">In my spare time I really enjoy hanging out with friends<br>and do something fun or exciting. And obviously sitting<br>in front of my computer with music in my ears.</p>
+			  	<p class="commenTxtLeft">I have always loved to explore and try out new things.<br>Being spontaneous is a big part of me. If i see a great opportunity<br> to try something out, count me in! It's important to me to keep<br>growing on a personal level together with my professional career.</p>
+			</div>
+
+			<div class="col-md-6 col-xs-12">
+				<img id="myPhoto" class="animated fadeInRight" src="images/alex.png" alt="Alexander Brasgaard" title="Alexander Brasgaard">
+			</div>
+		</div>
+	</div>
 </div>
 
-<!-- Footer start -->
-<footer class="globalFooter">
-	<section id="footerSoMe">
-		<p class="footerTxt"> This is my footer. This is my footer. This is my footer. This is my footer.</p>
-	</section>
-</footer>
+<div class="container-fluid-3">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-xs-12">
+				<h3 class="titleTxt">portfolio</h3>
+				<p class="underTitleTxt">Take a look at my portfolio which I'm proud to present.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-xs-12">
+				<h3 class="titleTxtLeft">haute friture</h3>
+			  	<p class="underTitleTxtLeft">Where grill meets gourment.</p>
+			  	<p class="commenTxtLeft">In this project I worked together with my study group to create<br>a new website for Haute Friture. They didn't have a website and<br> wanted to be able to show their current and new customers what<br>their gourment grill looked like. The requirement was to have focus on<br>their unique food through visuelle element and a menu card.</p>
+			</div>
+
+			<div class="col-md-6 col-xs-12">
+				<img class="portfolioPhoto" src="images/hautefriture.png" alt="Haute Friture Gourment Grill" title="Haute Friture Gourment Grill">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-xs-12">
+				<img class="portfolioPhoto" src="images/njordex.jpg" alt="Njordex" title="Njordex">
+			</div>
+
+			<div class="col-md-6 col-xs-12">
+				<h3 class="titleTxtRight">njordex</h3>
+			  	<p class="underTitleTxtRight">Denmarks first cryptocurrency exchange.</p>
+			  	<p class="commenTxtRight">In this project my assignment was to create the visual identity for Njordex.<br>This included a landing page and a full website design with 10+ sites.<br>The goal was to create an easy overview which instantly builded trust.<br>One of the biggest focuses was to keep the site user friendly everywhere.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid-6">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-xs-12">
+				<h3 class="titleTxtLeft">nordnordnord</h3>
+			  	<p class="underTitleTxtLeft">Upcoming danish indierock band.</p>
+			  	<p class="commenTxtLeft">Working with nordnordnord was something totally different.<br>The focus was to differentiate them from other bands and their identities.<br>The main problem was that nordnordnord really didn't know their own identity very well, since they were a new band. The end result was nordnordnord's landing page was animated in different layers.<br>This together with sound created a whole new music univers.</p>
+			</div>
+
+			<div class="col-md-6 col-xs-12">
+				<img class="portfolioPhoto" src="images/nordnordnord.png" alt="nordnordnord" title="nordnordnord">
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="container-fluid-7">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-xs-12">
+				<h3 class="titleTxt">contact</h3>
+			  	<p class="underTitleTxt">Here is the different places you can contact me</p>
+			  	<p class="underTitleTxt">+45 25 70 24 76 <br>Alexaderbrasgaard@hotmail.com</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid-8">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-xs-12">
+				<p class="commenTxt"> Copyright © Alexander Brasgaard 2018</p>
+			</div>
+		</div>
+	</div>
+</div>
 
 <!-- Scripts -->
 <script src="js/scripts.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -57,5 +57,3 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
-
-
